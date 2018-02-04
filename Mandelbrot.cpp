ddd1e38501abb2a8b5565dfab8d46845dec67b03
joelcre <m2creutz@gmail.com>
2018-02-04 20:50:35 +0100

    latest serial version

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -9,16 +9,10 @@ class Pixel
     double re,im;
     int pixelValue=-1;
 public:
-  Pixel(double a=0, double b=0)
+  Pixel(double a, double b)
   {
     re=a;
     im=b;
-  }
-  
-  Pixel set_pixel(double a, double b){
-    re=a;
-    im=b;
- 
   }
   int calcPixel(double b, int N)
   {
@@ -49,44 +43,17 @@ public:
 
 };
 int main(int argc, char *argv[]) {
-
-  double b=2;  // size of picture
-  int w=2048;// number of pixels along the x-axis
-  int h=2048;//number of pixels along the y-axis
-  double dx= (double) 2*b/(w-1); // increment in width
-  double dy= (double) 2*b/(h-1); // increment in height
-  double dreal; // temporary variable
-  double dimag;
-  FILE *fp;
-  fp = fopen("color.txt","w");
   int rc = MPI_Init(&argc,&argv);
   int size,rank;
   MPI_Status status;
+
   int hello = MPI_Comm_size(MPI_COMM_WORLD, &size);
   int hello2 = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  
-  
-  // very important to set the number of nodes to 1 when running this with mpirun! 
- Pixel * pixel_array;
-  
-  pixel_array= new Pixel [w*h];
 
-  for(int x=0;x<w;x++){ // for each column in picture
-    dreal=(double)x*dx-b;
-    for(int y=0;y<h;y++ ){ // for each row picture
-      dimag=(double)y*dy-b;
-      pixel_array[x*h+y].set_pixel(dreal,dimag);    
-      fprintf(fp, "%hhu", pixel_array[x*h+y].calcPixel(2.0,256)); // printing pixelvalue to file
-    fprintf(fp, "\n");
-    }
+  Pixel p(1/(1+(float) rank),1/(1+(float) rank));
+  std::cout <<"Rank: "<< rank <<'\n'<<p.calcPixel(2.0,256)<< "\n\n";
 
-    }
-
- 
-  
   MPI_Finalize();
-  fclose(fp);
   return 0;
-
 
 }
