81304322dc2a29ac1cb51c6f894d9ca09b390904
Maximilian Karlstrom <maxik@kth.se>
2018-02-18 20:33:38 +0100

    Lite test för att se om vi kan synkronisera processerna för att göra saker i ordning. Verkar fungera bra men ful kod.

--- HW 2/program.cpp
+++ HW 2/program.cpp
@@ -4,32 +4,19 @@
 
 using namespace std;
 
-int calcPix(double maxAbsoluteValue, int maxIter, double re, double im)
+double f(double x)
 {
-  double tempReal=0, tempImaginary=0,
-  nextReal,nextImaginary,currentAbsolute;
-  int iters=0;
-  double maxAbsoluteValueSquared=pow(maxAbsoluteValue,2);
-  do
-  {
-    iters++;
-    nextReal= re+pow(tempReal,2)-pow(tempImaginary, 2);
-    nextImaginary=im+2*tempReal*tempImaginary;
-    currentAbsolute=pow(nextReal,2)+pow(nextImaginary,2);
-    tempReal=nextReal;
-    tempImaginary=nextImaginary;
-  } while(currentAbsolute<maxAbsoluteValueSquared && iters<maxIter);
-  return iters;
+  return 1+x;
 }
 
 int main ( int argc, char *argv[] )
 {
   int rank,nProc,errorCode;
-  double b=2;
-  int N=256;
-  double minx=-0.5,maxx=0.5,miny=0.5,maxy=1.5;
-  int width=2048, height=2048;
-  double dx=(maxx-minx)/(width-1),dy=(maxy-miny)/(height-1);
+  int N=99, start=0, end=1;
+  double h=1/((double)N +1), uInitial=0, uEnd=0;
+
+
+
 
 
   //Initializing MPI and getting world size and rank of process.
@@ -37,51 +24,37 @@ int main ( int argc, char *argv[] )
   errorCode = MPI_Comm_size ( MPI_COMM_WORLD, &nProc );
   errorCode = MPI_Comm_rank ( MPI_COMM_WORLD, &rank );
 
-  //Checking of number of columns is dividable.
-  if (width%nProc!=0)
+  int go=1;
+  if(rank==0)
   {
-    std::cout << "Number of columns not evenly divisable by the number of processes.";
-    MPI_Finalize();
-    return 0;
-  }
-
-  //Some help variables to simplify code.
-  //Picture divided vertically into segments.
-  int partitionWidth=width/nProc;
-  int partitionSize=height*partitionWidth;
-  int resArray[partitionSize];
-
-  //Calculating certain part of the picture for certain processes.
-  for(int xIter=rank*partitionWidth;xIter<(rank+1)*partitionWidth; xIter++)
-  {
-    for(int yIter=0; yIter<height;yIter++)
-    {
-      //Saving the value calculated by the function calcPix.
-      resArray[(xIter%partitionWidth)*height+yIter]=
-        calcPix(b,N,((double)(xIter))*dx+minx,((double)(yIter))*dy+miny);
-    }
-  }
-
-  //Collecting all information
-  if (rank==0) {
-    FILE *fp;
-    fp = fopen("color.txt","w");
-
-    for (int i=0; i<nProc; i++)
-    {
-      //Fetching calculated arrays from other processes if not "master" process.
-      if(i!=0) errorCode=MPI_Recv(&resArray,partitionSize, MPI_INT,i,1,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      for (int j = 0;j < partitionSize; j++) {
-        fprintf(fp, "%hhu", resArray[j]); // printing pixelvalue to file
-        fprintf(fp, "\n");
+      std::cout << "I'm process nr: "<< rank+1<< '\n';
+      std::cout << "With points from " <<h*rank*(N+1)/nProc<<" to "<<h*(rank+1)*(N+1)/nProc<< '\n';
+      for (int i = rank*(N+1)/nProc;i <(rank+1)*(N+1)/nProc ; i++) {
+        std::cout << h*i <<", ";
       }
-    }
-    fclose(fp);
+      std::cout << '\n';
+
+      if(nProc>1)
+        MPI_Send(&go, 1, MPI_INT,rank+1,0,MPI_COMM_WORLD);
   }
-  if(nProc>1&&rank>0)
+  else
   {
-    MPI_Send(&resArray, partitionSize, MPI_INT,0,1,MPI_COMM_WORLD);
+    MPI_Recv(&go, 1, MPI_INT,rank-1,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    std::cout << "I'm process nr: "<< rank+1<< '\n';
+    std::cout << "With points from " <<h*rank*(N+1)/nProc<<" to "<<h*(rank+1)*(N+1)/nProc<< '\n';
+    for (int i = rank*(N+1)/nProc;i <(rank+1)*(N+1)/nProc ; i++) {
+      std::cout << h*i <<", ";
+    }
+    std::cout <<"\n\n";
+    if(rank<nProc-1)
+      MPI_Send(&go, 1, MPI_INT,rank+1,0,MPI_COMM_WORLD);
+
   }
+
+
+
+
+
   MPI_Finalize();
   return 0;
 }
