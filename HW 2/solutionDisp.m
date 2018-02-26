03436ba748a2e376f7d3122ddc6c0564d5772e88
Max <maxik@kth.se>
2018-02-26 10:19:40 +0100

    Programmet färdigt

--- HW 2/solutionDisp.m
+++ HW 2/solutionDisp.m
@@ -6,11 +6,12 @@ u=u';
 
 x=linspace(0,1,numel(u));
 h=x(2)-x(1);
+x2=x.^2;
 
 % Selected function f
-f=sin(5*x(2:end-1));
+f=sin(5*x2(2:end-1));
 % Selected function r
-r=-exp(x);
+r=x2-x-1;
 
 ubis=u(3:end)-2*u(2:end-1)+u(1:end-2);
 ubis=ubis/h^2;
@@ -21,12 +22,11 @@ ru=r.*u;
 rhs=ubis+ru(2:end-1);
 legend('Solution')
 print('Solution','-dpng')
+fclose('all');
 %%
 figure();
-plot(x(2:end-1),rhs);
-hold on
-plot(x(2:end-1),f)
-fclose('all');
+plot(x(2:end-1),rhs-f);
 
-legend('Approximate RHS', 'True, LHS')
+
+legend('Error')
 print('Error','-dpng')
